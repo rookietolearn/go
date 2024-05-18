3945a0df1cb44a413a7814cb43379407a1413313
wy <wyworkaccount@126.com>
2024-05-18 16:45:21 +0800

    context

--- context/context.go
+++ context/context.go
@@ -9,21 +9,31 @@ import (
 type Cmk struct {
 }
 
+//type CommonCtx struct {
+//	comctx context.Context
+//	mdctx  context.Context
+//}
+
 func main() {
 	tmap := map[string]string{
+		"locate":   "md", //md,sys
 		"playerID": "21334455",
 		"areaID":   "123",
 	}
 	ctx := context.TODO()
-	ctxCommon := NewContext(ctx, tmap)
-	ctxMetaData := NewMetaDataCtx(ctx, tmap)
+	//ctxCommon := NewContext(ctx, tmap)
+	//ctxMetaData := NewMetaDataCtx(ctx, tmap)
 
-	resp, _ := Get(ctxMetaData, `areaID`)
-	mdresp, _ := Mget(ctxCommon, "areaID")
+	cCtx := NewCommonCtx(ctx, tmap)
+
+	areaID := CommonGet(cCtx, "areaID")
+
+	//resp, _ := Get(ctxMetaData, `areaID`)
+	//mdresp, _ := Mget(ctxCommon, "areaID")
+
 	fmt.Println("======")
-	fmt.Println(resp)
+	fmt.Println(areaID)
 	fmt.Println("======")
-	fmt.Println(mdresp)
 	fmt.Println("======")
 
 }
@@ -34,6 +44,41 @@ func NewContext(ctx context.Context, v map[string]string) context.Context {
 
 func NewMetaDataCtx(ctx context.Context, v map[string]string) context.Context {
 	return metadata.NewContext(ctx, v)
+}
+
+func NewCommonCtx(ctx context.Context, v map[string]string) context.Context {
+	var commonCtx = context.TODO()
+	locate := v["locate"]
+	if locate == "md" {
+		md := metadata.NewContext(commonCtx, v)
+		commonCtx = context.WithValue(ctx, Cmk{}, md)
+	} else {
+		commonCtx = context.WithValue(ctx, Cmk{}, v)
+		metadata.NewContext(commonCtx, nil)
+	}
+	return commonCtx
+}
+
+func CommonGet(ctx context.Context, k string) string {
+	amap, ok := ctx.Value(Cmk{}).(map[string]string)
+	var resp string
+	//无metadata数据
+	if ok {
+		for k1, v := range amap {
+			if k == k1 {
+				resp = v
+			}
+		}
+	}
+	metaData, ok := ctx.Value(Cmk{}).(context.Context)
+	if ok {
+		mdresp, ok := metadata.Get(metaData, k)
+		if !ok {
+			return ""
+		}
+		resp = mdresp
+	}
+	return resp
 }
 
 func Mget(ctx context.Context, k string) (string, bool) {
